55db49528e7705db9318dff3aca6555eaf130e89
Rexford Nyarko <rexfordnyrk@gmail.com>
2022-11-30 17:39:50 +0000

    read config file with viper

--- cmd/root.go
+++ cmd/root.go
@@ -5,11 +5,11 @@ Copyright © 2022 Rexford A. Nyarko rexfordnyrk@gmail.com
 package cmd
 
 import (
+	"fmt"
 	"github.com/rexfordnyrk/webshot/capture"
-	"log"
-	"os"
-
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"os"
 )
 
 
@@ -22,6 +22,13 @@ var rootCmd = &cobra.Command{
 	Short: "Capture Screenshots of web pages.",
 	Long: `A simple CLI program that captures screenshots of web pages.
 You can have single URL screenshots or batch with a list of screenshots.`,
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		// PersistencePreRunE on the root command works well
+		return initViperConfig(cmd)
+	},
+	//can be removed after testing as it is not needed in this program
+	Run: func(cmd *cobra.Command, args []string) {
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -42,12 +49,34 @@ func init() {
 	rootCmd.PersistentFlags().IntVarP(&conf.Width, "width", "w",1440, "The width for both fullscreen and windowed screenshot.")
 	rootCmd.PersistentFlags().IntVarP(&conf.Height, "height", "H",900, "The height for a windowed screenshot. This option is ignored for a fullscreen capture.")
 	rootCmd.PersistentFlags().StringVarP(&conf.Format, "format", "f","png", "The format of the file output, values is either 'pdf' or 'png'")
+	rootCmd.PersistentFlags().StringP("config", "c","webshot.json", "Specify the config file name to load. It must be in a JSON format")
+	rootCmd.PersistentFlags().StringP("configDir", "d",".", "Specify the directory to find the config file to load. Default is the current directory")
 
-	err := conf.ValidateConfig()
-	if err != nil {
-		log.Println(err.Error())
-		os.Exit(1)
-	}
 }
 
+func initViperConfig(cmd *cobra.Command) error {
+	//initialize viper
+	v := viper.New()
 
+	//specifying the type of configuration file format to be read
+	v.SetConfigType("json")
+
+	// name of config file obtained from the --config or -c flag
+	v.SetConfigName(cmd.Flags().Lookup("config").Value.String())
+
+	//look for config in the User's Home directory
+	v.AddConfigPath(cmd.Flags().Lookup("configDir").Value.String())
+
+	// Find and read the config file
+	if err := v.ReadInConfig(); err != nil { // Handle errors reading the config file
+		// It doesn't matter if config file does not exist we will later try ENV variables
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return err
+		}
+	}
+
+	//temporarily log loaded config
+	fmt.Println("the settings",v.AllSettings())
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -1,10 +1,36 @@
 module github.com/rexfordnyrk/webshot
 
-go 1.16
+go 1.18
+
+require (
+	github.com/chromedp/chromedp v0.8.6
+	github.com/spf13/cobra v1.6.1
+)
 
 require (
 	github.com/chromedp/cdproto v0.0.0-20221126224343-3a0787b8dd28 // indirect
-	github.com/chromedp/chromedp v0.8.6 // indirect
-	github.com/spf13/cobra v1.6.1 // indirect
-	golang.org/x/sys v0.2.0 // indirect
+	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.1.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
+	github.com/spf13/afero v1.9.2 // indirect
+	github.com/spf13/cast v1.5.0 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.14.0 // indirect
+	github.com/subosito/gotenv v1.4.1 // indirect
+	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
+	golang.org/x/text v0.4.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
